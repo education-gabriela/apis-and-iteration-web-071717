ee87ec8cb9b3286dfb89edb3d78c0172e73939ec
dkush22 <dkush22@gmail.com>
2017-07-18 13:03:11 -0400

    printed movie list

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -13,14 +13,12 @@ def get_movies_from_api(movies_urls)
   movies = movies_urls.collect do |movie_url|
     call_api(movie_url)
   end
-  puts movies.first
   movies
 end
 
 
 
 def get_character_movies_from_api(character)
-  #make the web request
   next_url = "http://www.swapi.co/api/people/"
 
   while next_url != nil
@@ -29,43 +27,23 @@ def get_character_movies_from_api(character)
   
     character_result = get_character(films_hash["results"], character)
     if character_result
+      puts "Name: #{character_result["name"]}"
       movies = get_movies_from_api(character_result["films"])
-      return movies
+      return parse_character_movies(movies)
     end
   end
 
   "Character not found"
-
-  # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
 end
 
 def parse_character_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
+  films_hash.collect.with_index do |film, index|
+    puts "#{index + 1}. #{film["title"]} (Episode #{film["episode_id"]})" 
+  end
 end
 
 def show_character_movies(character)
   get_character_movies_from_api(character)
-  # next_url = "http://www.swapi.co/api/people/"
-
-  # while next_url != nil
-  #   films_hash = get_character_movies_from_api(character, next_url)
-  #   next_url = films_hash["next"]
-
-  #   character_result = get_character(films_hash["results"], character)
-  #   if character_result
-  #     return character_result
-  #   end
-  # end
-
-  # "Character not found"
 end
 
 def get_character(movies, search_term)
@@ -73,7 +51,4 @@ def get_character(movies, search_term)
     character["name"].start_with?(search_term)
   end
 end
-## BONUS
 
-# that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
-# can you split it up into helper methods?
